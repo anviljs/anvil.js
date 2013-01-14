7774a2eac82256572dd9c752ae6f86ac6d9ba143
Alex Robson <asrobson@gmail.com>
2013-01-13 23:07:46 -0600

    Add process and http host with update to config for support

--- src/host.js
+++ src/host.js
@@ -6,95 +6,13 @@ var fs = require( "fs" );
 var open = require( "open" );
 var url = require( "url" );
 
-var hostFactory = function( _, anvil ) {
+module.exports = function( _, anvil ) {
 	
 	var Host = function() {
 		this.clients = [];
+		this.compilers = {};
 		_.bindAll( this );
-		anvil.http = {
-			init: this.init,
-			registerPath: this.registerPath
-		};
 		this.topics = {};
-	};
-
-	Host.prototype.init = function( done ) {
-		var self = this;
-		
-		this.app = express();
-		this.app.use( express.bodyParser() );
-		this.app.use( this.app.router );
-
-		var extPath = path.resolve( __dirname, "../ext" );
-		self.registerPath( "/anvil", extPath );
-		this.server = http.createServer( this.app ).listen( anvil.config.port );
-		this.socketServer = socketIO.listen( this.server );
-		this.socketServer.set( "log level", 1 );
-		this.socketServer.sockets.on( "connection", this.addClient );
-		
-		_.each( anvil.extensions.widgets, function( widget, name ) {
-			var widgetPath = "/widgets/" + name + "/";
-			self.registerPath( widgetPath, widget.resourcePath );
-			_.each( widget.routes, function( spec, route ) {
-				_.each( spec, function( definition, operation ) {
-					self.registerRoute( widgetPath, route, definition, function( req, resp ) {
-						var envelope = {
-							data: req.body,
-							headers: req.headers,
-							params: {},
-							reply: function( envelope ) {
-								var code = envelope.statusCode || 200;
-								resp.send( 200, envelope.data );
-							}
-						};
-						for( var key in req.params ) {
-							var val = req.params[ key ];
-							if( envelope.data[ key ] ) {
-								envelope.params[ key ] = val;
-							} else {
-								envelope.data[ key ] = val;
-							}
-						}
-						definition.handle.apply( widget, [ envelope ] );
-					} );
-					self.registerTopic( name, route, definition, function( message, socket ) {
-						var envelope = {
-							data: message.data || message,
-							headers: message.headers || [],
-							reply: function( envelope ) {
-								socket.emit( message.replyTo || "trash", envelope );
-							}
-						};
-						definition.handle.apply( widget, [ envelope ] );
-					} );
-				} );
-			} );
-		} );
-	};
-
-	Host.prototype.registerPath = function( url, filePath ) {
-		console.log( "registering: " + url + " to " + filePath );
-		this.app.use( url, express[ "static" ]( path.resolve( filePath ) ) );
-	};
-
-	Host.prototype.registerRoute = function( url, route, definition, callback ) {
-		var fullUrl = url + route + ( definition.path || "" );
-		console.log( "registering: " + fullUrl + " with method " + definition.verb );
-		this.app[ definition.verb ]( fullUrl, callback );
-	};
-
-	Host.prototype.registerTopic = function( name, route, definition, callback ) {
-		var self = this,
-			topic = name + "." + route;
-		console.log( "registering topic: " + topic );
-		if( this.topics[ topic ] ) {
-			this.topics[ topic ].push( callback );
-		} else {
-			this.topics[ topic ] = [ callback ];
-		}
-		_.each( this.clients, function( client ) {
-			client.on( topic, function( data ) { callback( data, client ); } );
-		} );
 	};
 
 	Host.prototype.addClient = function( socket ) {
@@ -107,7 +25,211 @@ var hostFactory = function( _, anvil ) {
 				}
 			} );
 		} );
+		anvil.emit( "socket.client.connected", { socket: socket } );
 		socket.on( "end", this.removeClient );
+	};
+
+	Host.prototype.addCompiler = function( extension, mimeType, compiler ) {
+		this.compilers[ extension ] = {
+			extension: extension,
+			compiler: compiler,
+			mimeType: mimeType
+		};
+	};
+
+	Host.prototype.addMiddleware = function( callback ) {
+		var middleware = function( req, res, next ) {
+			if (req.method !== "GET") {
+				next();
+				return;
+			}
+			var writeHead = res.writeHead,
+				write = res.write,
+				end = res.end,
+				chunks = [],
+				body,
+				totalSize = 0;
+
+			res.write = function( chunk, encoding ) {
+				if ( typeof chunk === "string" ) {
+					var length;
+					if ( !encoding || encoding === 'utf8' ) {
+						length = Buffer.byteLength( chunk );
+					}
+					var buffer = new Buffer( length );
+					buffer.write( chunk, encoding );
+					chunks.push( buffer );
+				} else {
+					chunks.push( chunk );
+				}
+				totalSize += chunk.length;
+			};
+
+			res.end = function( chunk, encoding ) {
+				if ( chunk && chunk.length ) {
+					res.write( chunk, encoding );
+				}
+				body = new Buffer( totalSize );
+				var offset = 0;
+				chunks.forEach( function( chunk ) {
+					chunk.copy( body, offset );
+					offset += chunk.length;
+				});
+				res.writeHead = writeHead;
+				res.write = write;
+				res.end = end;
+				callback( body, req, res );
+			};
+			next();
+		};
+
+		this.app.use( middleware );
+	};
+
+	Host.prototype.addSocketInclude = function() {
+		var socketMiddleware = function( body, req, res ) {
+			var original = body.toString();
+			if( original.match( /[<][\/]body[>]/ ) ) {
+				var modified = original.replace( /[<][\/]body[>]/,
+					"<script src=\"/socket.io/socket.io.js\"></script>\n<script src=\"/anvil/buildHook.js\"></script>\n</body>"
+				);
+				res._headers[ "content-length" ] = modified.length;
+				res.end( modified );
+			} else {
+				res.end( body );
+			}
+		};
+		this.addMiddleware( socketMiddleware );
+	};
+
+	// yay, Jim Cowart to the rescue with this gem...
+	Host.prototype.buildUrl = function() {
+		var idx = 0,
+			cleaned = [];
+		while( idx < arguments.length ) {
+			var segment = arguments[ idx ];
+			if( segment.substr( 0, 1 ) === "/" ) {
+				segment = segment.substr( 1 );
+			}
+			if( segment.substr( segment.length-1, 1 ) === "/" ) {
+				segment = segment.substring( 0, segment.length - 1 );
+			}
+			if( !_.isEmpty( segment ) ) {
+				cleaned.push( segment );
+			}
+			idx++;
+		}
+		return "/" + cleaned.join('/');
+	};
+
+	Host.prototype.createRouteHandle = function( url, verb, resource, handle ) {
+		this.registerRoute( url, verb, function( req, resp ) {
+			var envelope = {
+				data: req.body,
+				headers: req.headers,
+				params: {},
+				reply: function( envelope ) {
+					var code = envelope.statusCode || 200;
+					resp.send( 200, envelope.data );
+				}
+			};
+			for( var key in req.params ) {
+				var val = req.params[ key ];
+				if( envelope.data[ key ] ) {
+					envelope.params[ key ] = val;
+				} else {
+					envelope.data[ key ] = val;
+				}
+			}
+			handle.apply( resource, [ envelope ] );
+		} );
+	};
+
+	Host.prototype.createTopicHandle = function( topic, resource, handle ) {
+		this.registerTopic( topic, function( message, socket ) {
+			var envelope = {
+				data: message.data || message,
+				headers: message.headers || [],
+				reply: function( envelope ) {
+					socket.emit( message.replyTo || "trash", envelope );
+				}
+			};
+			handle.apply( resource, [ envelope ] );
+		} );
+	};
+
+	Host.prototype.init = function() {
+		var self = this;
+		
+		this.app = express();
+		this.app.use( express.bodyParser() );
+		this.app.use( this.app.router );
+		this.addSocketInclude();
+
+		var extPath = path.resolve( __dirname, "../ext" );
+		self.registerPath( "/anvil", extPath );
+		this.server = http.createServer( this.app ).listen( anvil.config.port );
+		this.socketServer = socketIO.listen( this.server );
+		this.socketServer.set( "log level", 1 );
+		this.socketServer.sockets.on( "connection", this.addClient );
+
+		_.each( anvil.extensions.widgets, function( resource, name ) {
+			self.processResource( "widget", name, resource );
+		} );
+
+		_.each( anvil.config.httpPaths, function( filePath, url ) {
+			self.registerPath( url, filePath );
+		} );
+
+		_.each( this.compilers, this.registerCompiler );
+
+		anvil.on( "build.done", this.refreshClients );
+
+		if( anvil.config.browser ) {
+			this.open();
+		}
+	};
+
+	Host.prototype.refreshClients = function() {
+		anvil.log.event( "refreshing connected socket clients" );
+		this.notifyClients( "refresh", {} );
+	};
+
+	Host.prototype.registerPath = function( url, filePath ) {
+		anvil.log.debug( "registering: " + url + " to " + filePath );
+		this.app.use( url, express[ "static" ]( path.resolve( filePath ) ) );
+	};
+
+	Host.prototype.registerRoute = function( url, verb, callback ) {
+		anvil.log.debug( "registering: " + url + " with method " + verb );
+		this.app[ verb ]( url, callback );
+	};
+
+	Host.prototype.registerCompiler = function( compiler ) {
+		var pattern = new RegExp( compiler.extension + "$" );
+		this.app.get( pattern, function( req, res ) {
+			var fileName = "." + req.url,
+				ext = path.extname( fileName );
+			res.header( "Content-Type", compiler.mimeType );
+			anvil.fs.read( fileName, function( content ) {
+				compiler.compiler( content, function( compiled ) {
+					res.send( compiled );
+				} );
+			} );
+		} );
+	};
+
+	Host.prototype.registerTopic = function( topic, callback ) {
+		var self = this;
+		anvil.log.debug( "registering topic: " + topic );
+		if( this.topics[ topic ] ) {
+			this.topics[ topic ].push( callback );
+		} else {
+			this.topics[ topic ] = [ callback ];
+		}
+		_.each( this.clients, function( client ) {
+			client.on( topic, function( data ) { callback( data, client ); } );
+		} );
 	};
 
 	Host.prototype.removeClient = function( socket ) {
@@ -115,9 +237,24 @@ var hostFactory = function( _, anvil ) {
 		if( index >= 0 ) {
 			this.clients.splice( index, 1 );
 		}
+		anvil.emit( "socket.client.disconnected", { socket: socket } );
+	};
+
+	Host.prototype.processResource = function( prefix, name, resource ) {
+		var self = this,
+			resourcePath = self.buildUrl( prefix, name );
+		this.registerPath( resourcePath, resource.resourcePath );
+		_.each( resource.routes, function( spec, route ) {
+			_.each( spec, function( definition, operation ) {
+				var handle = definition.handle,
+					topic = name + "." + route,
+					url = self.buildUrl( prefix, name, route, ( definition.path || "" ) ),
+					verb = definition.verb;
+				self.createRouteHandle( url, verb, resource, handle );
+				self.createTopicHandle( topic, resource, handle );
+			} );
+		} );
 	};
 
 	return new Host();
 };
-
-module.exports = hostFactory;
--- src/processHost.js
+++ src/processHost.js
@@ -1,34 +1,186 @@
-var monitor = require( "forever-monitor" );
+var spawn = require( "child_process" ).spawn;
 
 module.exports = function( _, anvil ) {
 
 	var Process = function( id ) {
 		this.id = id;
 		this.goPostal( id );
+		this.config = anvil.config.processes[ id ];
+		this.running = false;
+		_.bindAll( this );
+	};
+
+	Process.prototype.allowRestart = function( file ) {
+		console.log( "check restart for change on " + file );
+		var ignore = this.config.ignore,
+			watch = this.config.watch,
+			noIgnore = _.isEmpty( ignore ),
+			noWatch = _.isEmpty( watch );
+
+		if( noIgnore && noWatch ) {
+			return true;
+		} else if( !noIgnore && this.contains( this.config.ignore, file ) ) {
+			return false;
+		} else if( !noWatch && this.contains( this.config.watch, file ) ) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+	Process.prototype.contains = function ( list, file ) {
+		var patterns = _.isArray( list ) ? list : [ list ];
+		console.log( patterns );
+		return _.any( patterns, function( pattern ) {
+			var matched = anvil.minimatch( file, pattern );
+			console.log( file + " ? " + pattern + " = " + matched );
+			return matched;
+		} );
 	};
 
 	Process.prototype.start = function() {
+		var self = this,
+			config = this.config,
+			handle = spawn(
+				config.command,
+				config.args,
+				{
+					cwd: config.cwd || process.cwd(),
+					stdio: config.stdio || "inherit",
+					env: config.env || process.env
+				} );
+		this.running = true;
 
+		handle.on( "exit", function( code, signal ) {
+			self.emit( "exit", { id: this.id, code: code } );
+			self.running = false;
+		} );
+		this.handle = handle;
+		return handle;
 	};
 
 	Process.prototype.stop = function() {
-
+		var self = this;
+		if( this.handle ) {
+			var signals = this.config.killSignal,
+				handle = this.handle;
+			if( _.isString( signals ) || _.isEmpty( ) ) {
+				signals = [ signals || "SIGTERM" ];
+			}
+			_.each( signals, function( signal ) {
+				if( handle ) {
+					anvil.log.debug( "Killing process " + self.id + " with " + signal );
+					handle.kill( signal );
+				}
+			} );
+		}
 	};
 
-	Process.prototype.start = function() {
-
+	Process.prototype.restart = function( changedFile ) {
+		var self = this;
+		if( !this.running ) {
+			this.start();
+		}
+		if( this.config.restart && this.allowRestart( changedFile ) ) {
+			this.once( "exit", function() {
+				setTimeout( function() {
+					self.start();
+				}, 200 );
+			} );
+			this.stop();
+		}
 	};
 
 	anvil.addEvents( Process );
 
 
 	var ProcessHost = function() {
+		var self = this,
+			shutdown;
+		this.config = anvil.config.processes;
+		this.processes = {};
+		this.lastChanged = "";
 
-		
+		shutdown = function() {
+			shutdown = function() {};
+			self.stop();
+			process.exit( 0 );
+		};
 
+		anvil.on( "plugins.configured", function() {
+			self.config = anvil.config.processes;
+		} );
+
+		anvil.on( "file.change", function( change ) {
+			console.log( change );
+			self.lastChanged = change.file;
+		} );
+
+		anvil.on( "build.done", function( args ) {
+			if( anvil.config.host ) {
+				anvil.log.step( "Build done, starting hosted processes" );
+				self.restart();
+			}
+		} );
+
+		process.on( "SIGINT", shutdown );
+		process.on( "SIGTERM", shutdown );
+		_.bindAll( this );
+	};
+
+	ProcessHost.prototype.start = function( id ) {
+		var self = this,
+			valid = this.config[ id ],
+			process = id ? this.processes[ id ] : undefined;
+		if( id == undefined ) {
+			_.each( this.processes, function( process, pid ) {
+				self.start( id );
+			} );
+		} else {
+			if( valid ) {
+				if( !process ) {
+					process = new Process( id );
+				}
+				process.start();
+			}
+		}
+	};
+
+	ProcessHost.prototype.stop = function( id ) {
+		var self = this,
+			process = id ? this.processes[ id ] : undefined;
+		if( id == undefined ) {
+			_.each( this.processes, function( process, pid ) {
+				self.stop( pid );
+			} );
+		} else {
+			if( process && process.running ) {
+				process.stop();
+			}
+		}
+	};
+
+	ProcessHost.prototype.restart = function( id ) {
+		var self = this,
+			valid = this.config[ id ],
+			process = id ? this.processes[ id ] : undefined;
+		if( id == undefined ) {
+			_.each( this.config, function( process, pid ) {
+				self.restart( pid );
+			} );
+		} else {
+			if( valid ) {
+				if( !process ) {
+					process = new Process( id );
+					process.start();
+					this.processes[ id ] = process;
+				} else {
+					process.restart( this.lastChanged );
+				}
+			}
+		}
 	};
 
 	anvil.addEvents( ProcessHost );
-
 	return new ProcessHost();
 };
